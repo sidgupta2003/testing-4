b5902866a5a0bc951d84c0a3ef4713845bfe028e
Siddharth Gupta <91104449+ded-abhishek@users.noreply.github.com>
2025-03-21 13:16:23 +0530

    changes

--- lmsproject/lmsproject/settings.py
+++ lmsproject/lmsproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,24 +79,46 @@ WSGI_APPLICATION = 'lmsproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+ENVIRONMENT = os.getenv('DJANGO_ENV', 'local')  # Default is 'local'
+
+# Load the correct .env file
+if ENVIRONMENT == 'production':
+    load_dotenv('.env.prod')
+else:
+    load_dotenv('.env.local')
+
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'lmsproject',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',  # Use the database server address if remote
+    #     'PORT': '3306',       # Default MySQL port
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_DATE,NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'",
+    #         'charset': 'utf8mb4',
+    #         'use_unicode': True,
+    #     },
+    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lmsproject',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',  # Use the database server address if remote
-        'PORT': '3306',       # Default MySQL port
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),  # Default MySQL port
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_DATE,NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'",
             'charset': 'utf8mb4',
             'use_unicode': True,
         },
     }
+
 }
 
 
